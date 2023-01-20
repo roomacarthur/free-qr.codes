b22af54a39a2a0f136e32ce1da59167e3aa15849
roomacarthur <roomacarthur@gmail.com>
2023-01-20 01:43:50 +0000

    added custom colours

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const generateQr = document.getElementById('generateSubmit')
 const qrcode = document.getElementById('qrcode')
 const spinner = document.getElementById('spinner')
 const letsGenerate = document.getElementById('letsGenerate')
-const donwload = document.getElementById('downloadbtn')
+const donwloadBtn = document.getElementById('downloadbtn')
 
 // Generate sequence to run on user submit.
 function qrCodeSubmit(e){
@@ -20,26 +20,42 @@ function qrCodeSubmit(e){
     clearQR()
     const url = document.getElementById('urlInput').value;
     const size = document.getElementById('size').value;
+    const color1 = document.getElementById('color1').value;
+    const color2 = document.getElementById('color2').value;
     loadSpinner();
     setTimeout(() =>{
         hideSpinner();
-        generateQrCode(url, size);
-        donwload.style.display = 'block';
-    }, 1000);
+        generateQrCode(url, size, color1, color2);
+        donwloadBtn.style.display = 'block';
+        setTimeout(() =>{
+            let qrUrl = qrcode.querySelector('img').src;
+            downloadNow(qrUrl);
+        },50);
+    }, 1500);
 
 };
-// Generate Qr Code!
-function generateQrCode(url,size){
+// Download Generated QR Code.
+function downloadNow(qrUrl){
+    const link = document.createElement('a');
+    link.classList = 'link-light'
+    link.id = 'downloadLink'
+    link.href = qrUrl
+    link.innerHTML = 'Download Now'
+    link.download = 'FREE-QR-CODES'
+    donwloadBtn.appendChild(link)
+}
+
+// Generate Qr Code! --- WORKS
+function generateQrCode(url,size,color1,color2){
     const newQr = new QRCode(qrcode, {
         text: url,
         width: size,
         height: size,
-        colorDark: '#000000',
-        colorLight: '#FFFFFF',
+        colorDark: color1,
+        colorLight: color2,
     });
 };
-
-// Clear QR before making a new one.
+// Clear QR before making a new one. --- WORKS
 function clearQR(){
     qrcode.innerHTML = ''
 }
